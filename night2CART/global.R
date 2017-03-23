aac71b9467bdecff4e8b897556bb0fb46e57d142
laderast <roshioshi@gmail.com>
2017-03-23 15:03:30 -0700

    modified global.R for cart data

--- night2CART/global.R
+++ night2CART/global.R
@@ -4,43 +4,20 @@ library(dplyr)
 library(party)
 
 ###Put Data processing steps here
-#load dataset A
-
-# datasetA <- read.delim("data/datasetA.txt",row.names = 1)
-# datasetA <- datasetA %>% filter(startWeights != 9999 & endWeights != 9999)
-# datasetA <- datasetA %>% mutate(weightLoss = startWeights - endWeights, weightLossPerDay = weightLoss / timeElapsed)
-# datasetA <- na.omit(datasetA)
-# dataset <- datasetA
+###This is a test using the iris dataset
+###You can load any data here, just name it "dataset"
 
 dataset <- iris
+outcomeVar <- "Species"
 dataset <- dataset %>% mutate(sepalRatio=Sepal.Length/Sepal.Width, petalRatio = Petal.Length/Petal.Width)
 
+##Build test and train sets of data
 trainIndex <- sample(nrow(dataset), size = floor(0.75 * nrow(dataset)))
 trainDataset <- dataset[trainIndex,]
 testDataset <- dataset[-trainIndex,]
 
-outcomeVar <- "Species"
-
+##Find covariate names
 covariateNames <- colnames(dataset)[!colnames(dataset) %in% outcomeVar]
-
-#Code to process dataset B
-
-#Code to combine the two datasets
-#Here we select columns for each dataset (treatment and weight)
-#in order to make the two datasets comparable, we make both genders lowercase 
-#(coverting it to a character), but then we have to recast it as a 
-#factor (categorical variable)
-# datasetAselect <- datasetA %>% mutate(site="A", gender=factor(tolower(gender))) %>% 
-#   select(treatment, weightLossPerDay, site, gender)
-# 
-# datasetBselect <- datasetB %>% mutate(site="B", gender=factor(tolower(gender))) %>% 
-#   select(treatment, weightLossPerDay, site, gender)
-
-#dataset <- rbind(datasetAselect, datasetBselect)
-
-#have to recast site as a factor because it's a character
-#dataset$site <- factor(dataset$site)
-
 
 ##Don't modify anything below here, or app won't work properly.
 #get the variable types

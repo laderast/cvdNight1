18193f5cfb5629b3e9398087ba34b43153bbd6ab
laderast <roshioshi@gmail.com>
2017-05-15 11:48:26 -0700

    update of slides

--- global.R
+++ global.R
@@ -5,12 +5,16 @@ library(data.table)
 
 ###Put Data processing steps here
 #load dataset
-dataset <- fread("data/fullPatientData.csv",stringsAsFactors = TRUE)
+#dataset <- fread("data/fullPatientData.csv",stringsAsFactors = TRUE)
+dataset <- fread("data/genoData.csv",stringsAsFactors = TRUE)
 dataset$patientID <- as.character(dataset$patientID)
 dataset$age <- ordered(dataset$age)
 
+
+
+
 ##Don't modify anything below here, or app won't work properly.
-#get the variable types
+##get the variable types
 varClass <- sapply(dataset, class)
 varClass <- lapply(varClass, function(x){if(length(x)>1)x <- x[2]; return(x)})
 
